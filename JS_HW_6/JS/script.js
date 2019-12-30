1cb5825844bed32b40ce6f64b4f45f5cf5dacdbd
annaglukhikh <53951413+annaglukhikh@users.noreply.github.com>
2019-12-30 20:06:33 +0500

    Add files via upload

--- JS_HW_6/JS/script.js
+++ JS_HW_6/JS/script.js
@@ -1,3 +1,6 @@
+/*
+old version
+
 const rock = 0;
 const scissors = 1;
 const paper = 2;
@@ -15,4 +18,33 @@ if(rock || scissors) {
   console.log('player win');
 }else {
   console.log('player win');
+}
+*/
+const rock = 0;
+const scissors = 1;
+const paper = 2;
+
+// попросим игрока ввести ответ в виде числа
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+// Рандомное число 0, 1 или 2 — разберем это позже, пока не заморачивайтесь
+let computer = Math.floor(Math.random() * 3);
+
+// опишем все условия и будем выводить в консоль 'computer win' или 'player win'
+
+if (player === computer){
+console.log("Tie!");
+}else if (player === 'paper' && computer === 'rock'){
+console.log("You win!");
+}else if (player === 'rock' && computer === 'scissors'){
+console.log("You win!");
+}else if (player === 'scissors' && computer === 'rock'){
+console.log("You lose!");
+}else if (player === 'rock' && computer === 'paper'){
+console.log("You lose!");
+}else if (player === 'paper' && computer === 'scissors'){
+console.log("You lose!");
+}else if (player === 'scissors' || computer === 'paper'){
+console.log("You win!");
+}else{
+console.log("Invalid input, please try again");
 }
