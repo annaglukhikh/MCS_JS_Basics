d88c616736bd80c7122d4b1f06791134ec6579a9
annaglukhikh <53951413+annaglukhikh@users.noreply.github.com>
2020-01-01 16:32:48 +0500

    Add files via upload

--- JS_HW_6/JS/script.js
+++ JS_HW_6/JS/script.js
@@ -1,5 +1,5 @@
 /*
-old version
+//version 1
 
 const rock = 0;
 const scissors = 1;
@@ -20,6 +20,9 @@ if(rock || scissors) {
   console.log('player win');
 }
 */
+
+/*
+//version 2
 const rock = 0;
 const scissors = 1;
 const paper = 2;
@@ -47,4 +50,51 @@ console.log("You lose!");
 console.log("You win!");
 }else{
 console.log("Invalid input, please try again");
+}
+*/
+
+// vertion 3
+const rock = 0;
+const scissors = 1;
+const paper = 2;
+
+// попросим игрока ввести ответ в виде числа
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+// Рандомное число 0, 1 или 2 — разберем это позже, пока не заморачивайтесь
+let computer = Math.floor(Math.random() * 3);
+
+// опишем все условия и будем выводить в консоль 'computer win' или 'player win'
+
+// побеждает игрок
+if (player === computer) {
+  console.log("tie");
+}else if (player === 2) {
+  if (computer === 0) {
+    console.log('player wins');
+  }
+}else if (player === 1) {
+  if (computer === 2) {
+    console.log('player wins');
+  }
+}else if (player === 0) {
+  if (computer === 1) {
+    console.log('player wins');
+}
+}
+
+//побеждает компьютер
+if (player === computer) {
+  console.log("tie");
+}else if (player === 0) {
+  if (computer === 2) {
+    console.log('computer wins');
+  }
+}else if (player === 1) {
+  if (computer === 0) {
+    console.log('computer wins');
+  }
+}else if (player === 2) {
+  if (computer === 1) {
+    console.log('computer wins');
+}
 }
