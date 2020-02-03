f439a0aa23341b4677227de47f30dab05edb2d0d
annaglukhikh <53951413+annaglukhikh@users.noreply.github.com>
2020-02-03 23:31:56 +0500

    Update script.js

--- JS_HW_10_5.2.1/JS/script.js
+++ JS_HW_10_5.2.1/JS/script.js
@@ -1,28 +1,8 @@
-/*console.log('You are at ' +window.location);
-
-function moduleSquare(num) {
-  return Math.abs(num)/2;
-}
-
-console.log(moduleSquare(-3));
-console.log(moduleSquare(3));
-
-console.log(Math.floor(2.678));
-console.log(Math.round(2.678));
-
-console.log(Math.random());
-
-console.log(Math.random()*(10-5)+5);
-
-console.log(Math.floor(Math.random()*(10-5)+5));
-
-console.log(Math.floor(Math.random()*(11-1)+1));
-*/
 //Напишите функцию, которая на вход получает число, потом возводит его в случайную целую степень от 1 до 10 (не включая 10), а потом возвращает модуль результата при помощи метода abs() объекта Math
 //  return Math.abs(Math.pow(resultNumber, Math.floor(Math.random()*(11-1)+1)));
 
 let resultNumber = prompt('insert number here');
 let number = (resultNumber) => {
-  return Math.abs(Math.pow(resultNumber, Math.floor(Math.random()*(11-1)+1)));
+  return Math.abs(Math.pow(resultNumber, Math.floor(Math.random()*9+1)));
 }
 console.log(number(resultNumber));
