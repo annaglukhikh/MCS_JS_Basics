7f07c6944dbad4c373292ee26021610a88b74fe8
annaglukhikh <53951413+annaglukhikh@users.noreply.github.com>
2020-02-04 22:22:42 +0500

    Update script.js

--- JS_HW_15_6.4.1/JS/script.js
+++ JS_HW_15_6.4.1/JS/script.js
@@ -1,35 +1,30 @@
-const APIKey = 'dac1a2350a8ff9e3b24b83c7e158f1b0';
-//const city = 'Екатеринбург';
-
-let city = document.querySelector('input');
-
-const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
-
-let xhr = new XMLHttpRequest();
-
-xhr.open('GET', url, false);
-
-xhr.send();
+const form = document.forms[0];
+const insert = document.querySelector('.tempValue');
 
 form.onsubmit = function(s) {
+  s.preventDefault();
+
+  let cityName = document.getElementById("cityName").value;
+
+  let city = cityName;
+
+  const APIKey = 'dac1a2350a8ff9e3b24b83c7e158f1b0';
+
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
+
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, false);
+
+  xhr.send();
+
   if(xhr.status != 200) {
     console.log(xhr.status + ' '+ xhr.statusText);
   } else {
     let DATA = JSON.parse(xhr.responseText);
     console.log(DATA);
-    //document.write(DATA.main.temp - 273)
+
     let div = document.querySelector('div');
     div.innerHTML = (DATA.main.temp - 273);
   }
 }
-/*
-if(xhr.status != 200) {
-  console.log(xhr.status + ' '+ xhr.statusText);
-} else {
-  let DATA = JSON.parse(xhr.responseText);
-  console.log(DATA);
-  //document.write(DATA.main.temp - 273)
-  let div = document.querySelector('div');
-  div.innerHTML = (DATA.main.temp - 273);
-}
-*/
