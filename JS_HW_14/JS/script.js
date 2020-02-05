92064971716f7f43502aad2a827788547a1bdb3c
annaglukhikh <53951413+annaglukhikh@users.noreply.github.com>
2020-02-05 17:33:30 +0500

    Update script.js

--- JS_HW_14/JS/script.js
+++ JS_HW_14/JS/script.js
@@ -14,27 +14,35 @@ name : "Litecoin",
 price: 140
 }
 ]
-
-/* for(key in crypto) {
-  for(price in crypto[key]) {
-    console.log(crypto[key][price]);
-  }
-}
-*/
-//let maxLength = 0;
-//for (i = 0; i < 100000; i++) {
-//  let currentPrice = crypto.find(item => item.price <= 3000);
-//  console.log(currentPrice);
-//}
-
-// Создайте страницу, демонстрирующую сравнение курсов криптовалюты — Заголовок берем из name — Цену пишем в параграф — Устанавливаем на странице цветной блок, ширина которого привязана к цене валюты
-
-let maxPrice = crypto.find(item => item.price <= 100000);
+//ищем максимальную цену во всех объектах массива
+let maxPrice = Math.max.apply(Math, crypto.map(function(n) { return n.price; }));
+//выводим в консоль
 console.log(maxPrice);
 
-let strokeLength = Object.keys(maxPrice)
+//создаём массив, куда поместим все значения ключа "цена"
+let newArr = [];
 
-console.log(strokeLength);
-//консоль: 
-//{name: "Bitcoin", price: 1388.37}
-//script.js:37 (2) ["name", "price"]
+//создаём цикл, берём найденное максимальное значение цены за 100%. И от него считаем процены по остальным ценам. Добавляем полученное в новый массив
+for (i = 0; i < crypto.length; i++) {
+  let lengthPercent = (crypto[i].price/maxPrice);
+  console.log(lengthPercent);
+  newArr.push(lengthPercent);
+}
+
+//на всякий случай выводим в консоль, чтобы убедиться, что процены рассчитаны верно
+console.log(newArr);
+
+//создаём константы со шкалами, которые в div
+const scaleBit = document.querySelector('.scaleOne');
+const scaleEth = document.querySelector('.scaleTwo');
+const scaleLit = document.querySelector('.scaleThree');
+
+//создаём константы с числами, которые вытаскиваем из нового массива
+const bit = Number(newArr[0]);
+const eth = Number(newArr[1]);
+const lit = Number(newArr[2]);
+
+//делаем pineapple-applepen с шириной div и константами, которые были в массиве. Т.к. в bit, eth, lit находятся проценты, умножаем их на 100, чтобы получить число. Это можно было и раньше сделать, но я не шипко сообразительная.
+scaleBit.style.width = bit*100 + 'vw';
+scaleEth.style.width = eth*100 + 'vw';
+scaleLit.style.width = lit*100 + 'vw';
